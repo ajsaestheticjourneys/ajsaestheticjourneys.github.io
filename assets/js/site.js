3c416a7d9f20339d5ce963d501a0e5812843f59f
ajsaestheticjourneys <ajsdynamicdashboards@gmail.com>
2025-05-02 12:55:26 +0100

    re-added site.js

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,4 +1,16 @@
+/**
+ * Menu
+ */
+$("a.menu-icon").on("click", function(event) {
+  var w = $(".menu");
 
+  w.css({
+    display: (w.css("display") === "none")
+      ? "block"
+      : "none"
+  });
+});
+    
 /**
  * Footer year
  */
